3802269a1b75552baa008b252ca000e6c366c07a
chrissierra <christopher.sierra@usach.cl>
2019-03-13 15:11:02 -0300

    asdf

--- wp-content/plugins/chrisplugin/chrisplugin.php
+++ wp-content/plugins/chrisplugin/chrisplugin.php
@@ -752,7 +752,6 @@ function woocommerce_transbank_init()
      **/
     function woocommerce_add_transbank_gateway($methods)
     {
-        
         $methods[] = 'WC_Gateway_transbank';
         return $methods;
     }
@@ -761,7 +760,9 @@ function woocommerce_transbank_init()
      * Muestra detalle de pago a Cliente a finalizar compra
      **/
     function pay_content($order_id)
-    {
+    {           
+      header("location:https://google.com");
+
         $order_info = new WC_Order($order_id);
         $transbank_data = $_POST;
 
